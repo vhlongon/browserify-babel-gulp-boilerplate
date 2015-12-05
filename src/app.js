468b8930626969adabff49435dc3d188d00b0e9d
vhlongon <victor.longon@zooma.se>
2015-12-06 00:49:11 +0100

    right changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import {Person} from './includes/Person';
 import * as test from './includes/test';
-let $ = require('jquery');
+var $ = require('jquery');
 require('browsernizr/test/fullscreen-api');
 require('browsernizr/test/css/columns');
-let Modernizr = require('browsernizr');
+var Modernizr = require('browsernizr');
 
-global.app = function main() {
-  let person = new Person('Jerry Lee', 'Lewis');
-  console.log(person.fullName);
+global.app = function () {
+    let person = new Person('Victor', 'Longon');
+    console.log(person.fullName);
+    $('body').append('Some text');
 };
